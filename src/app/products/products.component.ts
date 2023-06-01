0314252927db28c34d7955b7cf78a6ab194e150e
zoya_rassadkinaO <zoya_rassadkina@epam.com>
2023-06-01 09:42:52 +0300

    add hooks and linter

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ProductsService } from './products.service';
 import { Observable } from 'rxjs';
 import { Product } from './product.interface';
@@ -8,12 +8,9 @@ import { Product } from './product.interface';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent implements OnInit {
-  readonly products$: Observable<
-    Product[]
-  > = this.productsService.getProducts();
+export class ProductsComponent {
+  readonly products$: Observable<Product[]> =
+    this.productsService.getProducts();
 
   constructor(private readonly productsService: ProductsService) {}
-
-  ngOnInit(): void {}
 }
